9ffd163cb0b6f4867e492ce7faace15f390ce16d
Guru <guru28272@gmail.com>
2024-06-01 20:18:43 +0530

    changes

--- src/utils/useWhatsOnYourMind.js
+++ src/utils/useWhatsOnYourMind.js
@@ -9,7 +9,7 @@ const useWhatsOnYourMind = (id) => {
 
   const fetchData = async () => {
     const res = await fetch(
-      `https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0826802&lng=80.2707184&collection=${id}&tags=layout_CCS_Chinese&sortBy=&filters=&type=rcv2&offset=0&page_type=null`
+      `https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0826802&lng=80.2707184&collection=${id}&tags=layout_CCS_Chinese&sortBy=&filters=&type=rcv2&offset=0&page_type=null`
     );
     const json = await res.json();
 
